f194179bd4ff3058026cd76eadf7e8ff067ff614
Rahul Mane <rahulmane23@gmail.com>
2026-06-23 20:45:32 +0530

    Fixed login role and name

--- server/server.js
+++ server/server.js
@@ -14,21 +14,36 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
+// Connect Database
 connectDB();
 
-app.use(cors());
+// CORS FIX
+app.use(
+  cors({
+    origin: [
+      "https://name-hospital-management-frontend.onrender.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
+
+// Middleware
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("🏥 Hospital Management API is running...");
 });
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/patients", patientRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
